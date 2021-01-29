b6a7050b52c0a25dfb3c0a16fe093aac504eff8f
Mitch Harding <mitchell.harding@hpe.com>
2021-01-29 14:05:00 -0600

    CASMCMS-6608: Remove obsolete/broken cruft from cmsdev tool
    (cherry picked from commit 5013b59946b8c6f868215d68a265c7046df7ddf0)

--- cmsdev/internal/cmd/test.go
+++ cmsdev/internal/cmd/test.go
@@ -22,20 +22,9 @@ import (
 	"time"
 )
 
-// stage4 CT test timeouts (in seconds)
+// Test timeouts (in seconds)
 // (test will not retry after this amount of time)
-//
-// Currently the following tests only consist of
-// checks of k8s pods and their statuses:
-// bos, cfs, conman, ipxe, tftp, vcs
-// Because these checks are the ones most often
-// to succeed after retry (as the services are
-// starting up during stage4), we give those
-// components longer retry times.
-//
-// Note that once CASMCMS-5657 is implemented,
-// this will no longer be necessary.
-var Stage4CTTestTimeouts = map[string]int64{
+var TestTimeouts = map[string]int64{
 	"bos":    300,
 	"cfs":    300,
 	"conman": 300,
@@ -46,36 +35,36 @@ var Stage4CTTestTimeouts = map[string]int64{
 	"gitea":  300,
 }
 
-// Default stage4 CT test timeout (in seconds)
+// Default test timeout (in seconds)
 // This value is used if service is not listed in previous map
-const DefaultStage4CTTestTimeout int64 = 60
+const DefaultTestTimeout int64 = 60
 
-// Run the specified CT test
-func RunCTTest(service, stage string, local, smoke, ct bool) bool {
+// Run the specified test
+func RunTest(service string) bool {
 	switch service {
 	case "bos":
-		return bos.IsBOSRunning(local, smoke, ct, stage)
+		return bos.IsBOSRunning()
 	case "cfs":
-		return cfs.IsCFSRunning(local, smoke, ct, stage)
+		return cfs.IsCFSRunning()
 	case "conman":
-		return con.IsConmanRunning(local, smoke, ct, stage)
+		return con.IsConmanRunning()
 	case "crus":
-		return crus.IsCRUSRunning(local, smoke, ct, stage)
+		return crus.IsCRUSRunning()
 	case "ims":
-		return ims.IsIMSRunning(local, smoke, ct, stage)
+		return ims.IsIMSRunning()
 	case "ipxe", "tftp":
-		return ipxe_tftp.AreTheyRunning(local, smoke, ct, stage)
+		return ipxe_tftp.AreTheyRunning()
 	case "vcs", "gitea":
-		return vcs.IsVCSRunning(local, smoke, ct, stage)
+		return vcs.IsVCSRunning()
 	}
 	common.Usagef("Programming logic error: this line should never be reached. Invalid service (%s), but it should already have been validated!", service)
 	return false
 }
 
 func GetTimeout(service string) (timeout int64) {
-	timeout, ok := Stage4CTTestTimeouts[service]
+	timeout, ok := TestTimeouts[service]
 	if !ok {
-		timeout = DefaultStage4CTTestTimeout
+		timeout = DefaultTestTimeout
 	}
 	return
 }
@@ -107,23 +96,16 @@ func GetSleepDuration(attempt int, stopTime, timeout int64) time.Duration {
 var testCmd = &cobra.Command{
 	Use:   "test",
 	Short: "cms test services command",
-	Long: `test command runs cms ct, smoke, and api services tests.
+	Long: `test command runs service tests.
 Example Commands:
 
-cmsdev test conman --ct
-  # runs conman ct tests at default level 4
-cmsdev test tftp --ct --logs
-  # runs tftp ct tests with logging enabled
-cmsdev test ims --ct --crayctlStage=2
-  # runs ims ct tests at crayctl stage level 2
-cmsdev test bos --api
-  # runs entire bos api test suite
-cmsdev test cfs --smoke --verbose
-  # runs cfs smoke tests with verbosity`,
+cmsdev test conman
+  # runs conman tests
+cmsdev test tftp --logs -q
+  # runs tftp tests in quiet mode with logging enabled
+cmsdev test cfs -n --verbose
+  # runs cfs tests with verbosity and no retry on failure`,
 	Run: func(cmd *cobra.Command, args []string) {
-		// Variables for deprecated command line options
-		stage := "4"
-		local := false
 
 		// TODO: add custom flag validation
 		if len(args) < 1 {
@@ -132,135 +114,80 @@ cmsdev test cfs --smoke --verbose
 		service := strings.TrimSpace(args[0])
 
 		// TODO: pass these flags as args in a more elegant way
-		api, _ := cmd.Flags().GetBool("api")
-		ct, _ := cmd.Flags().GetBool("ct")
-		smoke, _ := cmd.Flags().GetBool("smoke")
 		logs, _ := cmd.Flags().GetBool("logs")
 		logsDir, _ := cmd.Flags().GetString("output")
 		noRetry, _ := cmd.Flags().GetBool("no-retry")
+		retry, _ := cmd.Flags().GetBool("retry")
+		quiet, _ := cmd.Flags().GetBool("quiet")
 		verbose, _ := cmd.Flags().GetBool("verbose")
 
 		// create log file if logs, ignore logsDir if !logs
-		common.CreateLogFile(logsDir, stage, service, api, ct, local, logs, smoke, verbose)
+		common.CreateLogFile(logsDir, service, logs, noRetry, retry, quiet, verbose)
 
 		// do some command line args checking
 		if validService := common.StringInArray(service, common.CMSServices); !validService {
 			common.Usagef("supported cms services are %s", strings.Join(common.CMSServices, " "))
-		} else if !(api || ct || smoke) {
-			common.Usagef("argument required, available cms tests types are: %s",
-				strings.Join(common.CMSServicesTestTypes, " "))
+		} else if retry && noRetry {
+			common.Usagef("--no-retry and --retry are mutually exclusive")
+		} else if quiet && verbose {
+			common.Usagef("--quiet and --verbose are mutually exclusive")
 		}
 
-		if ct {
-			// Initialize variables related to saving CT test artifacts
-			common.InitArtifacts(stage, service)
+		// If no-retry is specified, then retry must be false after previous check
+		// If no-retry is not specified, then we will still not retry, unless retry is set to true
+		// So this should get us what we want:
+		noRetry = !retry
 
-			if noRetry {
-				if RunCTTest(service, stage, local, smoke, ct) {
-					common.Success()
-				} else {
-					common.Failure()
-				}
+		// Initialize variables related to saving CT test artifacts
+		common.InitArtifacts(service)
+
+		if noRetry {
+			if RunTest(service) {
+				common.Success()
 			} else {
-				// CASMCMS-4429, CASMCMS-4574: Add automatic retries for CT tests
-				// We will sleep between retries until the test passes or we exceed the maximum
-				// retry time for that test.
-				testPassed, finalTry := false, false
-				timeout := GetTimeout(service)
-				common.Infof("Test retry timeout is %d seconds", timeout)
-				stopTime := timeout + time.Now().Unix()
-				for n := 1; ; n++ {
-					if (time.Now().Unix() + 1) >= stopTime {
-						common.Infof("Final attempt")
-						finalTry = true
-					} else {
-						common.Infof("Attempt #%d", n)
-					}
-					common.SetRunSubTag(strconv.Itoa(n))
-					testPassed = RunCTTest(service, stage, local, smoke, ct)
-					common.UnsetRunSubTag()
-					if testPassed {
-						common.Success()
-					} else if finalTry {
-						common.Failure()
-					} else if time.Now().Unix() >= (stopTime + 30) {
-						common.Failuref("Not retrying because stop time has already been exceeded by at least 30 seconds")
-					}
-					sleepDuration := GetSleepDuration(n, stopTime, timeout)
-					common.Infof("Attempt failed; waiting %v before retrying", sleepDuration)
-					time.Sleep(sleepDuration)
-				}
+				common.Failure()
 			}
 		}
 
-		switch service {
-		case "bos":
-			if ct || smoke {
-				_ = bos.IsBOSRunning(local, smoke, ct, stage)
-			} else { // api
-				if len(args) == 2 {
-					if !common.StringInArray(args[1], bos.APITestTypes) {
-						common.Usagef("argument required, current supported api tests are: %s\n%s\n",
-							strings.Join(bos.APITestTypes, " "),
-							"remove arguments to run entire tests suite")
-					}
-					bos.RunAPITests(local, args[1])
-				} else {
-					bos.RunAPITests(local)
-				}
+		// We will sleep between retries until the test passes or we exceed the maximum
+		// retry time for that test.
+		testPassed, finalTry := false, false
+		timeout := GetTimeout(service)
+		common.Infof("Test retry timeout is %d seconds", timeout)
+		stopTime := timeout + time.Now().Unix()
+		for n := 1; ; n++ {
+			if (time.Now().Unix() + 1) >= stopTime {
+				common.Infof("Final attempt")
+				finalTry = true
+			} else {
+				common.Infof("Attempt #%d", n)
 			}
-		case "cfs":
-			if ct || smoke {
-				_ = cfs.IsCFSRunning(local, smoke, ct, stage)
-			} else { // api
-				common.Usagef("no api tests currently found for %s\n", service)
+			common.SetRunSubTag(strconv.Itoa(n))
+			testPassed = RunTest(service)
+			common.UnsetRunSubTag()
+			if testPassed {
+				common.Success()
+			} else if finalTry {
+				common.Failure()
+			} else if time.Now().Unix() >= (stopTime + 30) {
+				common.Failuref("Not retrying because stop time has already been exceeded by at least 30 seconds")
 			}
-		case "conman":
-			if ct || smoke {
-				_ = con.IsConmanRunning(local, smoke, ct, stage)
-			} else { // api
-				common.Usagef("no api tests currently found for %s\n", service)
-			}
-		case "crus":
-			if ct || smoke {
-				_ = crus.IsCRUSRunning(local, smoke, ct, stage)
-			} else { // api
-				common.Usagef("no api tests currently found for %s\n", service)
-			}
-		case "ims":
-			if ct || smoke {
-				_ = ims.IsIMSRunning(local, smoke, ct, stage)
-			} else { // api
-				common.Usagef("no api tests currently found for %s\n", service)
-			}
-		case "ipxe", "tftp":
-			if ct || smoke {
-				_ = ipxe_tftp.AreTheyRunning(local, smoke, ct, stage)
-			} else { // api
-				common.Usagef("no api tests currently found for %s\n", service)
-			}
-		case "vcs", "gitea":
-			if ct || smoke {
-				_ = vcs.IsVCSRunning(local, smoke, ct, stage)
-			} else { // api
-				common.Usagef("no api tests currently found for %s\n", service)
-			}
+			sleepDuration := GetSleepDuration(n, stopTime, timeout)
+			common.Infof("Attempt failed; waiting %v before retrying", sleepDuration)
+			time.Sleep(sleepDuration)
 		}
-		common.Infof("cmsdev completed")
+
+		common.Errorf("PROGRAMMING LOGIC ERROR: This line should never be reached")
 		return
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(testCmd)
-	testCmd.Flags().Bool("api", false, "api tests")
-	testCmd.Flags().Bool("ct", false, "ct tests")
-	testCmd.Flags().Bool("smoke", false, "run smoke tests")
-	// TODO: ensure that these flags are persistent
-	testCmd.Flags().BoolP("logs", "", false, "enable test script logging")
-	testCmd.Flags().BoolP("no-retry", "n", false, "disable retry on failure, requires --ct")
-	testCmd.Flags().StringP("output", "o", "", "output logging to a file, requires --logs")
+	testCmd.Flags().BoolP("logs", "", false, "enable logging to file")
+	testCmd.Flags().BoolP("no-retry", "n", false, "no retry on failure (default)")
+	testCmd.Flags().BoolP("retry", "r", false, "retry on failure")
+	testCmd.Flags().StringP("output", "o", "", "specify log directory (requires --logs)")
+	testCmd.Flags().BoolP("quiet", "q", false, "quiet mode")
 	testCmd.Flags().BoolP("verbose", "v", false, "verbose mode")
-	testCmd.Flags().BoolP("local", "l", false, "no effect (deprecated)")
-	testCmd.Flags().String("crayctl-stage", "4", "no effect (deprecated)")
 }
--- ct-tests/crus_stage4_ct_tests.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# © Copyright 2020 Hewlett Packard Enterprise Development LP
-cmsdev test crus --ct --logs
-exit $?
