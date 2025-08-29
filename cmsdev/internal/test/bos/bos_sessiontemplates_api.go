edd2d14ec237a35e6cb5840998eb8e8c3f4cf5e7
Rahul <kumar.rahul2@hpe.com>
2025-08-29 13:25:37 -0400

    CASMCMS-9537: cmsdev: Remove misleading error message from BOS test
    (cherry picked from commit 94dbc68c2ff213dd0c566810a23790a282dcefea)
    
    Co-authored-by: Mitch Harding <mitchell.harding@hpe.com>

--- cmsdev/internal/test/bos/bos_sessiontemplates_api.go
+++ cmsdev/internal/test/bos/bos_sessiontemplates_api.go
@@ -432,7 +432,7 @@ func GetTenantFromList() string {
 	// Set the tenant name to be used in the tests
 	tenantName, err := common.GetRandomStringFromList(tenantList)
 	if err != nil {
-		common.Errorf("Error getting random tenant from list: %s", err.Error())
+		common.Debugf("Unable to get random tenant from list: %s", err.Error())
 		return ""
 	}
 	common.Infof("Using tenant: %s", tenantName)
